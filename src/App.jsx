ee5b2264b65b78d87320d740b11814bd99cee80f
Malinda Prabath <malindaprabath876@gmail.com>
2025-05-27 16:12:39 +0530

    completed full project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
-
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CreateContact from "./pages/CreateContact";
+import ViewContacts from "./pages/ViewContacts";
+import UpdateContact from "./pages/UpdateContact";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<CreateContact />} />
+        <Route path="/view" element={<ViewContacts />} />
+        <Route path="/update/:id" element={<UpdateContact />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
